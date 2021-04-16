b044830832c66276bfc6e66b6e051f5012ddfede
ohnitakahiro <ohnitakahiro@gmail.com>
2021-04-16 18:30:35 +0900

    コミット
    コミット

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState,useEffect } from 'react'
+
+function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() =>{
+    console.log();
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[])
+  return (
+    <div className="App">
+      <h1>フロントエンド</h1>
+      <p>{ message }</p>
+    </div>
+  );
+}
+
+export default App;
